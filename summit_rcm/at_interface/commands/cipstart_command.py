2d65266883a6b0da504c75c5a7927c1c0e614892
John Wozencraft <John.Wozencraft@lairdconnect.com>
2023-03-30 11:24:29 -0400

    summit-rcm: Various updates

--- summit_rcm/at_interface/commands/cipstart_command.py
+++ summit_rcm/at_interface/commands/cipstart_command.py
@@ -8,35 +8,58 @@ from summit_rcm.at_interface.connection_service import ConnectionService
 class CIPSTARTCommand(Command):
     name = "Start IP connection"
     signature = "at+cipstart"
+    valid_num_params = [4, 5]
 
     @staticmethod
     def execute(params: str) -> Tuple[bool, str]:
-        params_list = params.split(",")
-        num_params = len(params_list)
-        if num_params < 4 or num_params > 5:
-            return (True, "\r\nERROR\r\n")
-
-        connection_id = int(params_list[0])
-        type = params_list[1]
-        remote_ip = params_list[2]
-        remote_port = params_list[3]
-        enable_keepalive = params_list[4] if num_params == 5 else False
-
-        connection_type = ConnectionService.parse_connection_type(type)
-        if connection_type is None:
-            return (True, "\r\nERROR\r\n")
+        (valid, params_dict) = CIPSTARTCommand.parse_params(params)
+        if not valid:
+            return (
+                True,
+                f"\r\nInvalid Parameters: See Usage - {CIPSTARTCommand.signature}?\r\n",
+            )
+        params_dict["type"] = ConnectionService.parse_connection_type(
+            params_dict["type"]
+        )
+        if params_dict["type"] is None:
+            return (True, f"\r\nCONNECTION TYPE {params_dict[type]} ERROR\r\n")
 
         if ConnectionService().start_connection(
-            id=connection_id,
-            type=connection_type,
-            addr=remote_ip,
-            port=remote_port,
-            enable_keepalve=enable_keepalive,
+            id=params_dict["connection_id"],
+            type=params_dict["type"],
+            addr=params_dict["remote_ip"],
+            port=params_dict["remote_port"],
+            enable_keepalive=params_dict["enable_keepalive"],
         ):
             return (True, "\r\nOK\r\n")
         else:
-            return (True, "\r\nERROR\r\n")
+            return (True, "\r\nCONNECTION START ERROR\r\n")
+
+    @staticmethod
+    def parse_params(params: str) -> Tuple[bool, dict]:
+        valid = True
+        params_dict = {}
+        params_list = params.split(",")
+        given_num_param = len(params_list)
+        valid |= given_num_param in CIPSTARTCommand.valid_num_params
+        for param in params_list:
+            valid |= param != ""
+        if valid:
+            try:
+                params_dict["connection_id"] = int(params_list[0])
+                params_dict["type"] = params_list[1]
+                params_dict["remote_ip"] = params_list[2]
+                params_dict["remote_port"] = params_list[3]
+                params_dict["enable_keepalive"] = (
+                    params_list[4] if given_num_param == 5 else False
+                )
+            except Exception:
+                valid = False
+        return (valid, params_dict)
 
     @staticmethod
     def usage() -> str:
-        return "\r\nAT+CIPSTART=<connection id>,<type>,<remote IP>,<remote port>[,<enable keepalive>]\r\n"
+        return (
+            "\r\nAT+CIPSTART=<connection id>,<type>,<remote IP>,"
+            "<remote port>[,<enable keepalive>]\r\n"
+        )
