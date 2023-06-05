0df507fb7c7fb51d4b479e6bb5862a229e9d34db
John Wozencraft <John.Wozencraft@lairdconnect.com>
2023-06-05 14:42:21 -0400

    Summit-RCM: AT Interface: Avoid Network Service Name Collision
    Bug 24449

--- summit_rcm/at_interface/commands/connect_command.py
+++ summit_rcm/at_interface/commands/connect_command.py
@@ -1,10 +1,18 @@
+"""
+File that consists of the ActivateConnection Command Functionality
+"""
 from typing import List, Tuple
 from syslog import syslog, LOG_ERR
 from summit_rcm.at_interface.commands.command import Command
-from summit_rcm.services.network_manager_service import NetworkManagerService
+from summit_rcm.services.network_service import NetworkService
+from summit_rcm.services.network_service import ConnectionProfileNotFoundError
 
 
 class ActivateConnectionCommand(Command):
+    """
+    AT Command to activate or deactivate a connection profile
+    """
+
     NAME: str = "Activate/Deactivate Connection"
     SIGNATURE: str = "at+acon"
     VALID_NUM_PARAMS: List[int] = [2]
@@ -18,10 +26,19 @@ class ActivateConnectionCommand(Command):
                 f"\r\nInvalid Parameters: See Usage - {ActivateConnectionCommand.SIGNATURE}?\r\n",
             )
         try:
-            await activate_connection(params_dict["uuid"], params_dict["activate"])
+            try:
+                uuid = await NetworkService().get_connection_profile_uuid_from_id(
+                    params_dict["uuid"]
+                )
+            except ConnectionProfileNotFoundError:
+                uuid = params_dict["uuid"]
+            if params_dict["activate"]:
+                await NetworkService().activate_connection_profile(uuid=uuid)
+            else:
+                await NetworkService().deactivate_connection_profile(uuid=uuid)
             return (True, "\r\nOK\r\n")
-        except Exception as e:
-            syslog(LOG_ERR, f"Error Activating Connection {str(e)}")
+        except Exception as exception:
+            syslog(LOG_ERR, f"Error Activating Connection: {str(exception)}")
             return (True, "\r\nERROR\r\n")
 
     @staticmethod
@@ -43,7 +60,7 @@ class ActivateConnectionCommand(Command):
 
     @staticmethod
     def usage() -> str:
-        return "\r\nAT+ACON=<uuid>,<activate>\r\n"
+        return "\r\nAT+ACON=<uuid>|<id>,<activate>\r\n"
 
     @staticmethod
     def signature() -> str:
@@ -52,77 +69,3 @@ class ActivateConnectionCommand(Command):
     @staticmethod
     def name() -> str:
         return ActivateConnectionCommand.NAME
-
-
-async def activate_connection(uuid: str, activate: int):
-    connection_obj_path = await NetworkManagerService().get_connection_obj_path_by_uuid(
-        uuid
-    )
-    if connection_obj_path is None or connection_obj_path == "":
-        raise Exception("UUID not found")
-
-    if activate:
-        connection_props = await NetworkManagerService().get_connection_settings(
-            connection_obj_path
-        )
-        connection_setting_connection = connection_props.get("connection", {})
-        if connection_setting_connection.get("type", None) is None:
-            raise Exception("Unable to read connection settings")
-
-        if connection_setting_connection["type"].value == "bridge":
-            await NetworkManagerService().activate_connection(
-                connection_obj_path, "/", "/"
-            )
-            return
-        else:
-            interface_name = (
-                connection_setting_connection["interface-name"].value
-                if connection_setting_connection.get("interface-name", None) is not None
-                else ""
-            )
-            if interface_name == "":
-                raise Exception(
-                    "Could not find valid interface for the connection profile"
-                )
-
-            all_devices = await NetworkManagerService().get_all_devices()
-            for dev_obj_path in all_devices:
-                dev_props = await NetworkManagerService().get_obj_properties(
-                    dev_obj_path,
-                    NetworkManagerService().NM_DEVICE_IFACE,
-                )
-
-                dev_interface_name = dev_props.get("Interface", None)
-                if dev_interface_name is None:
-                    continue
-
-                if dev_interface_name == interface_name:
-                    await NetworkManagerService().activate_connection(
-                        connection_obj_path, dev_obj_path, "/"
-                    )
-                    return
-
-            raise Exception("appropriate device not found")
-
-    else:
-        manager_props = await NetworkManagerService().get_obj_properties(
-            NetworkManagerService().NM_CONNECTION_MANAGER_OBJ_PATH,
-            NetworkManagerService().NM_CONNECTION_MANAGER_IFACE,
-        )
-        active_connection_obj_paths = manager_props.get("ActiveConnections", [])
-
-        for active_connection_obj_path in active_connection_obj_paths:
-            active_connection_props = await NetworkManagerService().get_obj_properties(
-                active_connection_obj_path,
-                NetworkManagerService().NM_CONNECTION_ACTIVE_IFACE,
-            )
-
-            active_connection_uuid = active_connection_props.get("Uuid", None)
-            if active_connection_uuid is None:
-                continue
-
-            if uuid == active_connection_uuid:
-                await NetworkManagerService().deactivate_connection(
-                    active_connection_obj_path
-                )
-                return
