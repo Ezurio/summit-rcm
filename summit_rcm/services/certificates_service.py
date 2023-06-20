355b463b3b26755d3dbd835da3ad7ee10afb7aee
Chris Trowbridge <chris.trowbridge@lairdconnect.com>
2023-06-20 09:56:10 -0400

    summit-rcm: Decouple certificates logic and support password-less PKCS#12 certs
    Bug 23993

---
+++ summit_rcm/services/certificates_service.py
@@ -0,0 +1,37 @@
+"""
+Module to interact with the certificates.
+"""
+
+import os
+from syslog import syslog, LOG_ERR
+from typing import Optional, Tuple
+import openssl_extension
+from summit_rcm import definition
+
+
+class CertificatesService:
+    """Service to interact with the certificates."""
+
+    @staticmethod
+    def get_cert_info(
+        cert_name: str, password: Optional[str] = None
+    ) -> Tuple[dict, str]:
+        """
+        Retrieve the basic meta data info about the given certificate name from the certificates
+        managed by Summit RCM/NetworkManager.
+
+        Return value is a tuple in the form (cert_info, info_msg)
+        """
+
+        cert_file_path = f"{str(definition.FILEDIR_DICT.get('cert'))}/{cert_name}"
+
+        if not os.path.exists(cert_file_path):
+            return ({}, f"Cannot find certificate with name {cert_name}")
+
+        try:
+            cert_info = openssl_extension.get_cert_info(cert_file_path, password)
+            return (cert_info, "")
+        except Exception as exception:
+            error_msg = f"{str(exception)}"
+            syslog(LOG_ERR, error_msg)
+            return ({}, error_msg)
