6bcfe67940f93037c377132f7475a150f641c3c5
Chris Trowbridge <chris.trowbridge@lairdconnect.com>
2023-09-21 16:44:36 -0400

    summit-rcm: Add root Bluetooth endpoints and usage scripts
    Also, fix issue with restricted path resolution for v2 Bluetooth endpoints and
    remove unnecessary imports in bt.py.
    
    Bug 25078

--- summit_rcm/rest_api/legacy/bluetooth.py
+++ summit_rcm/rest_api/legacy/bluetooth.py
@@ -4,10 +4,33 @@
 from summit_rcm.bluetooth.bt_rest_service import BluetoothRESTService
 
 
+class BluetoothLegacyResource:
+    """
+    Resource to handle legacy requests when no device or controller is specified which is just a
+    wrapper around the common BluetoothRESTService request handler
+    """
+
+    async def on_get(self, req, resp):
+        """
+        GET handler for the /bluetooth endpoint
+        """
+        resp = await BluetoothRESTService.handle_get(
+            req, resp, None, None, is_legacy=True
+        )
+
+    async def on_put(self, req, resp):
+        """
+        PUT handler for the /bluetooth endpoint
+        """
+        resp = await BluetoothRESTService.handle_put(
+            req, resp, None, None, is_legacy=True
+        )
+
+
 class BluetoothControllerLegacyResource:
     """
-    Resource to handle legacy controller requests when no device which is just a wrapper around the
-    common BluetoothRESTService request handler
+    Resource to handle legacy controller requests when no device is specified which is just a
+    wrapper around the common BluetoothRESTService request handler
     """
 
     async def on_get(self, req, resp, controller):
