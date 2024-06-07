3cc5cdac9d0dea5556c2cf9c4276ed190e57ed3c
Service Build Slave <it.devops@lairdconnect.com>
2024-06-07 13:43:03 +0000

    bump to build number 10.229.0.19

--- summit_rcm/definition.py
+++ summit_rcm/definition.py
@@ -6,7 +6,7 @@ from summit_rcm.services.network_manager_service import (
     NMDeviceType,
 )
 
-SUMMIT_RCM_VERSION = "10.229.0.18"
+SUMMIT_RCM_VERSION = "10.229.0.19"
 
 # TODO - deal with directories for file retrieval (see FIELDIR_DICT and files.py)
 SYSTEM_CONF_DIR = "/data/"
