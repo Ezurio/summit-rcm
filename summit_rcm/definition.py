61bd63abd5f6b60639c460f55a43cbb39022fbad
Service Build Slave <it.devops@lairdconnect.com>
2024-06-19 23:49:01 +0000

    bump to build number 12.0.0.35

--- summit_rcm/definition.py
+++ summit_rcm/definition.py
@@ -7,7 +7,7 @@ from summit_rcm.services.network_manager_service import (
     NMDeviceType,
 )
 
-SUMMIT_RCM_VERSION = "12.0.0.33"
+SUMMIT_RCM_VERSION = "12.0.0.35"
 
 # TODO - deal with directories for file retrieval (see FIELDIR_DICT and files.py)
 SYSTEM_CONF_DIR = "/etc"
