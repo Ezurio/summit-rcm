d6295e64690a687b22cc5afc97b878350007d2d3
Service Build Slave <it.devops@ezurio.com>
2024-09-28 19:39:20 +0000

    Bump to build number 12.0.0.97

--- summit_rcm/definition.py
+++ summit_rcm/definition.py
@@ -11,7 +11,7 @@ from summit_rcm.services.network_manager_service import (
     NMDeviceType,
 )
 
-SUMMIT_RCM_VERSION = "12.0.0.96"
+SUMMIT_RCM_VERSION = "12.0.0.97"
 
 # TODO - deal with directories for file retrieval (see FIELDIR_DICT and files.py)
 SYSTEM_CONF_DIR = "/etc"
