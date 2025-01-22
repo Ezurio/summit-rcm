ca361e4afe7457dc1c73a389fdad2cc4c00efb7f
Chris Trowbridge <chris.trowbridge@ezurio.com>
2025-01-22 16:30:12 -0500

    feat(REST: network: interfaces): Add station dump endpoint
    This change introduces new REST endpoints (legacy and v2) to mimic the
    functionality of "iw dev <interface> station dump" via Netlink using pyroute2.
    
    * legacy endpoint: /networkInterfaceStationDump?name=<name>
    * v2 endpoint: /api/v2/network/interfaces/{name}/stationDump
    
    Also included are updates to the live REST API documentation to describe the
    response parameters.
    
    Finally, a set of usage example shell scripts are added as well.
    
    Refs: PROD-6307

---
+++ usage_examples/legacy/networkinterfacestationdump_get.sh
@@ -0,0 +1,15 @@
+##
+## SPDX-License-Identifier: LicenseRef-Ezurio-Clause
+## Copyright (C) 2024 Ezurio LLC.
+##
+IFNAME="${IFNAME:-"wlan0"}"
+source ../global_settings
+
+echo "========================="
+echo "network interface station dump"
+${CURL_APP} -s --header "Content-Type: application/json" \
+    --request GET \
+    ${URL}/networkInterfaceStationDump?name=${IFNAME} \
+    -b cookie -c cookie --insecure \
+| ${JQ_APP}
+echo -e "\n"
