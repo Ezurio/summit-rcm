0ab5f248e56eb5703a59257001067434af1ac140
Chris Trowbridge <chris.trowbridge@ezurio.com>
2025-01-24 15:55:40 -0500

    feat(REST: network: interface): Add available AP channels endpoint
    This change introduces new REST endpoints (legacy and v2) to support returning
    the subset of channels available for use in AP mode.
    
    - legacy endpoint: `/networkInterfaceAvailableApChannels`
    - v2 endpoint: `/api/v2/network/interfaces/{name}/availableApChannels`
    
    The criteria for an "available" channel is:
    - Marked as "supported" (i.e., like in `iw phy0 info`)
    - Not marked as "disabled" (i.e., like in `iw phy0 info`)
    - Not marked as "DFS" (i.e., like in `iw reg get`)
    - Not marked as requiring passive-scan (i.e., like in `iw reg get`)
    
    Also included are updates to the live REST API documentation to describe the
    response parameters.
    
    Additionally, a set of usage example shell scripts are added as well.
    
    Refs: PROD-6309

---
+++ usage_examples/v2/network_interfaces_available_ap_channels_get.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+##
+## SPDX-License-Identifier: LicenseRef-Ezurio-Clause
+## Copyright (C) 2025 Ezurio LLC.
+##
+
+NAME="${NAME:-"wlan0"}"
+
+source ../global_settings
+
+echo "========================="
+echo "Get the available AP mode channels for the network interface"
+echo "========================="
+echo
+
+echo "Interface Name: ${NAME}"
+echo -n "Status Code: "
+
+curl -s --location \
+    -w "%{http_code}\nResponse:\n" \
+    --request GET ${URL}/api/v2/network/interfaces/${NAME}/availableApChannels \
+    -b cookie -c cookie --insecure \
+    -o >(${JQ_APP})
+
+wait
