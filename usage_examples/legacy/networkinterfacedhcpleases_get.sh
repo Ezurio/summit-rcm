ede55091fc479d7ec3ff9b297fd0c95d22ed9977
Chris Trowbridge <chris.trowbridge@ezurio.com>
2025-02-07 14:56:41 -0500

    feat(network: interfaces): Add DHCP leases endpoint
    This change introduces new REST endpoints (legacy and v2) to support returning
    the set of DHCP leases which have been handed out by dnsmasq (the DHCP server
    used in conjunction with NetworkManager when the 'ipv4.method' setting is set
    to 'shared' on a connection profile).
    
    - legacy endpoint: `/networkInterfaceDhcpLeases`
    - v2 endpoint: `/api/v2/network/interfaces/{name}/dhcpLeases`
    
    The DHCP leases are read from the per-interface leasefile located at
    `/var/lib/NetworkManager/dnsmasq-{name}.leases` which is maintained by dnsmasq.
    
    Also included are updates to the live REST API documentation to describe the
    response parameters.
    
    Additionally, a set of usage example shell scripts are added as well.
    
    Refs: PROD-6308

---
+++ usage_examples/legacy/networkinterfacedhcpleases_get.sh
@@ -0,0 +1,16 @@
+##
+## SPDX-License-Identifier: LicenseRef-Ezurio-Clause
+## Copyright (C) 2025 Ezurio LLC.
+##
+
+IFNAME="${IFNAME:-"wlan0"}"
+source ../global_settings
+
+echo "========================="
+echo "network interface current DHCP leases"
+${CURL_APP} -s --header "Content-Type: application/json" \
+    --request GET \
+    ${URL}/networkInterfaceDhcpLeases?name=${IFNAME} \
+    -b cookie -c cookie --insecure \
+| ${JQ_APP}
+echo -e "\n"
